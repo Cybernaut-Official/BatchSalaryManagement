8dfc58140dd6e2ff8051c098619d9b060d1e89fc
ahn1305 <ahnashwin1305@gmail.com>
2025-03-13 11:08:51 +0530

    testing data persistency

--- salary_management/settings.py
+++ salary_management/settings.py
@@ -83,6 +83,17 @@ DATABASES = {
 }
 
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'postgres',
+#         'USER': 'ahn1305',
+#         'PASSWORD': 'csgo12345#',
+#         'HOST': 'guboshjsgbiobdewqknf.supabase.co',
+#         'PORT': '5432',
+#     }
+# }
+
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
